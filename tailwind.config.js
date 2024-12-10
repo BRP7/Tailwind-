5583473f2082ae146a9161a758e1ef2e17462d6a
BRP7 <bhumiparmar0007@gmail.com>
2024-12-10 23:00:31 +0530

    portfilio page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,20 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
+      animation: {
+        pulse: "pulse 2s infinite",
+      },
+      keyframes: {
+        pulse: {
+          "0%, 100%": { opacity: 0.5 },
+          "50%": { opacity: 1 },
+        },
+      },
       colors: {
         lightGray: "#f5f5f5",
         neonBlue: "#00d4ff",
-        whiteGlow : "#FCFDF8",
-        grayBlue : "#787C97",
+        whiteGlow: "#FCFDF8",
+        grayBlue: "#787C97",
         // neonBlue: '#66fcf1',
         // darkGray: '#1f2833',
         // lightGray: '#c5c6c7',
