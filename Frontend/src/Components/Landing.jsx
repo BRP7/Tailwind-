0b673103040df50c73248fceb1979793c88c0d01
BRP7 <bhumiparmar0007@gmail.com>
2024-12-14 23:55:51 +0530

    Landing Page

---
+++ Frontend/src/Components/Landing.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Landing() {
+  return (
+    <div
+      className="h-screen bg-cover bg-center flex items-center justify-center"
+      style={{
+        backgroundImage: "url('/image.jpg')",
+      }}
+    >
+    </div>
+  );
+}
+
+export default Landing;
