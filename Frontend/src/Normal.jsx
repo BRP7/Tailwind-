702a9e870588fb513e174ae96e04c7b9831a6441
BRP7 <bhumiparmar0007@gmail.com>
2025-01-01 22:59:53 +0530

    Normal Page

---
+++ Frontend/src/Normal.jsx
@@ -0,0 +1,63 @@
+import Landing from "./Components/Landing";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Portfolio from "./Components/Portfolio";
+// import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Rating from "./Components/Rating";
+// import LoginRegisterPage from "./Components/LoginRegisterPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
+
+export default function Normal(){
+
+  const testimonials = [
+    {
+      image: "myimag.png",
+      projectName: "Project One",
+      rating: "⭐⭐⭐⭐⭐",
+      feedback: "This project is amazing! Highly recommend.",
+    },
+    {
+      image: "myimag.png",
+      projectName: "Project Two",
+      rating: "⭐⭐⭐⭐",
+      feedback: "Great experience working on this!",
+    },
+    {
+      image: "myimag.png",
+      projectName: "Project Three",
+      rating: "⭐⭐⭐⭐⭐",
+      feedback: "Exceeded all my expectations!",
+    },
+    {
+      image: "myimag.png",
+      projectName: "Project Four",
+      rating: "⭐⭐⭐⭐⭐",
+      feedback: "Wonderful collaboration, loved the result!",
+    },
+    {
+      image: "myimag.png",
+      projectName: "Project Five",
+      rating: "⭐⭐⭐⭐",
+      feedback: "Really good work and easy to communicate!",
+    },
+  ];
+
+  return(
+    <>
+    {/* <Router>
+    </Router> */}
+    <Navbar />
+      <Landing/>
+      <Portfolio/>
+      <Rating testimonials={testimonials} />
+      <About/>
+      <Contact/>
+      <Footer/>
+    </>
+  )
+}
+  
